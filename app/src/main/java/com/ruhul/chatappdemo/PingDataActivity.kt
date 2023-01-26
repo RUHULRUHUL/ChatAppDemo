feda8ef8d7c53a09ca72b0205ac11ae4901b21b6
md Ruhul <ruhulamin122017@gmail.com>
2023-01-26 11:31:05 +0600

    timeStamp fixed

--- app/src/main/java/com/ruhul/chatappdemo/PingDataActivity.kt
+++ app/src/main/java/com/ruhul/chatappdemo/PingDataActivity.kt
@@ -1,18 +1,20 @@
 package com.ruhul.chatappdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.ruhul.chatappdemo.databinding.ActivityGetUserDataBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PingDataActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGetUserDataBinding
     private lateinit var database: DatabaseReference
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,18 +32,22 @@ class PingDataActivity : AppCompatActivity() {
             Log.d("time", "time = $time")
 
             val ping = Ping(
-                ServerValue.TIMESTAMP,
-                "",
-                binding.brodCastIdEdiText.text.toString(),
-                binding.fcmToken.text.toString(),
-                "Online",
+                    ServerValue.TIMESTAMP,
+                    "",
+                    binding.brodCastIdEdiText.text.toString(),
+                    binding.fcmToken.text.toString(),
+                    "Online",
             )
-            database.child("ping2").child(binding.userId.text.toString().trim())
-                .setValue(ping)
+            database.child("ping").child(binding.userId.text.toString().trim())
+                    .setValue(ping).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(this, "ping Data Save Success", Toast.LENGTH_SHORT).show()
+                        }
+                    }
         }
 
         binding.getButton.setOnClickListener {
-           val id = binding.userId.text.toString().trim()
+            val id = binding.userId.text.toString().trim()
             getData(id)
         }
 
@@ -49,20 +55,40 @@ class PingDataActivity : AppCompatActivity() {
 
     private fun getData(id: String) {
 
-        database.child("ping2").child(id)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    Log.d("msgLog", dataSnapshot.value.toString())
-                    val ping: Ping? = dataSnapshot.getValue(Ping::class.java)
-                    if (ping != null) {
-                        binding.brodCastIdEdiText.setText(ping.broadcast_id)
+        database.child("ping").child(id)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        Log.d("msgLog", dataSnapshot.value.toString())
+                        val ping: PingModelGet? = dataSnapshot.getValue(PingModelGet::class.java)
+                        timeConvertToDate(ping?.host_last_updated_at)
+
+                        if (ping != null) {
+                            binding.brodCastIdEdiText.setText(ping.broadcast_id)
+                        }
                     }
-                }
 
-                override fun onCancelled(error: DatabaseError) {
-                    Log.d("msgLog", "Error msg = ${error.message}")
-                }
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.d("msgLog", "Error msg = ${error.message}")
+                    }
 
-            })
+                })
+    }
+
+    private fun timeConvertToDate(hostLastUpdatedAt: Long?) {
+
+        if ((hostLastUpdatedAt != null) && (hostLastUpdatedAt > 0)) {
+            try {
+                val timeFormat = SimpleDateFormat("hh:mm a", Locale.getDefault())
+                val dateFormat = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+
+                val time: String = timeFormat.format(hostLastUpdatedAt)
+                val date: String = dateFormat.format(hostLastUpdatedAt)
+                Log.d("timeConvertToDate", "date: $date $time")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/ruhul/chatappdemo/PingModelGet.java
@@ -0,0 +1,60 @@
+package com.ruhul.chatappdemo;
+
+public class PingModelGet {
+    private Long host_last_updated_at;
+    private String device_id;
+    private String broadcast_id;
+    private String fcm_token;
+    private String status;
+
+    public PingModelGet() {
+    }
+
+    public PingModelGet(Long host_last_updated_at, String device_id, String broadcast_id, String fcm_token, String status) {
+        this.host_last_updated_at = host_last_updated_at;
+        this.device_id = device_id;
+        this.broadcast_id = broadcast_id;
+        this.fcm_token = fcm_token;
+        this.status = status;
+    }
+
+    public Long getHost_last_updated_at() {
+        return host_last_updated_at;
+    }
+
+    public void setHost_last_updated_at(Long host_last_updated_at) {
+        this.host_last_updated_at = host_last_updated_at;
+    }
+
+    public String getDevice_id() {
+        return device_id;
+    }
+
+    public void setDevice_id(String device_id) {
+        this.device_id = device_id;
+    }
+
+    public String getBroadcast_id() {
+        return broadcast_id;
+    }
+
+    public void setBroadcast_id(String broadcast_id) {
+        this.broadcast_id = broadcast_id;
+    }
+
+    public String getFcm_token() {
+        return fcm_token;
+    }
+
+    public void setFcm_token(String fcm_token) {
+        this.fcm_token = fcm_token;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
